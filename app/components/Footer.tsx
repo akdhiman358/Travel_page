5387ae1da4228e3edd9c5d9bb4bec05a4c2eb9fe
Akshay <akdiman358@gmail.com>
2024-05-10 10:14:04 +0530

    fixed bugs in the camp and footer files

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '../constants'
+
 const Footer = () => {
   return (
     <footer className="flexCenter mb-24">
@@ -12,11 +13,11 @@ const Footer = () => {
           </Link>
 
           <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
-            {FOOTER_LINKS.map((columns) => (
-              <FooterColumn title={columns.title}>
+            {FOOTER_LINKS.map((columns, index) => (
+              <FooterColumn title={columns.title} key={index}>
                 <ul className="regular-14 flex flex-col gap-4 text-gray-30">
-                  {columns.links.map((link) => (
-                    <Link href="/" key={link}>
+                  {columns.links.map((link, idx) => (
+                    <Link href="/" key={idx}>
                       {link}
                     </Link>
                   ))}
@@ -26,10 +27,10 @@ const Footer = () => {
 
             <div className="flex flex-col gap-5">
               <FooterColumn title={FOOTER_CONTACT_INFO.title}>
-                {FOOTER_CONTACT_INFO.links.map((link) => (
+                {FOOTER_CONTACT_INFO.links.map((link, idx) => (
                   <Link
                     href="/"
-                    key={link.label}
+                    key={link.label + idx}
                     className="flex gap-4 md:flex-col lg:flex-row"
                   >
                     <p className="whitespace-nowrap">
@@ -46,8 +47,8 @@ const Footer = () => {
             <div className="flex flex-col gap-5">
               <FooterColumn title={SOCIALS.title}>
                 <ul className="regular-14 flex gap-4 text-gray-30">
-                  {SOCIALS.links.map((link) => (
-                    <Link href="/" key={link}>
+                  {SOCIALS.links.map((link, idx) => (
+                    <Link href="/" key={idx}>
                       <Image src={link} alt="logo" width={24} height={24} />
                     </Link>
                   ))}
