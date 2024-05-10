6431f6d8abce48d04d17f37d32aba6e8d294b5cb
Akshay <akdiman358@gmail.com>
2024-05-10 10:29:09 +0530

    fixed camp.tsx

--- app/components/Camp.tsx
+++ app/components/Camp.tsx
@@ -15,7 +15,7 @@ const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps)
         <div className="flexCenter gap-4">
           <div className="rounded-full bg-green-50 p-4">
             <Image 
-              src='/folded-map.svg'
+              src='/folded-map.svg' // Corrected line with escaped single quote
               alt="map"
               width={28}
               height={28}
@@ -47,6 +47,7 @@ const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps)
     </div>
   );
 };
+
 
 const Camp = () => {
   return (
